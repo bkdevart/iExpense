a3da95ed2890e658f6fd5235101510eb4b3aa2f5
Brandon Knox <bknox.dev@gmail.com>
2021-03-25 20:03:38 -0700

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Brandon Knox on 3/25/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
